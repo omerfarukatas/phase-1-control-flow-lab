443e22857995184b2e322d8d3ce397fa0398141a
Omer <omerfarukatas96@gmail.com>
2022-04-11 22:17:59 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+  if (number <= 400){
+    return "This one is on me!";
+  } else if (400 < number, number < 2000){
+    return "That will be twenty bucks.";
+  } else if (2000 < number, number < 2500){
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+// console.log(scuberGreetingForFeet(55555));
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
+
+console.log(switchOnCharmFromTip("sup"));
